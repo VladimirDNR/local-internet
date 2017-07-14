c0e7d0bb2e221ac53e259bea9653ea367238c2db
vladimir <vladimir.dnr@yandex.ru>
2017-07-14 22:56:05 +0300

    Вторая страница. Ajax работает

--- page2.js
+++ page2.js
@@ -1,26 +1,57 @@
-if (!window.WebSocket) {
-    document.getElementById('res').innerHTML = 'WebSocket в этом браузере не поддерживается.';
-}
+// if (!window.WebSocket) {
+//     document.getElementById('res').innerHTML = 'WebSocket в этом браузере не поддерживается.';
+// }
+//
+// var socket = new WebSocket("ws://localhost:8081");
+//
+// socket.onmessage = function(event) {
+//     var incomingMessage = event.data;
+//     showMessage(incomingMessage);
+// };
+//
+// function showMessage(message) {
+//     var res = JSON.parse(message);
+//     var mesWebsocket = 'Ответ приехал по Websocket:<br>';
+//     mesWebsocket += 'Фамилия: ' + res.surname + '.<br>';
+//     mesWebsocket += 'Имя: ' + res.name + '.<br>';
+//     mesWebsocket += 'Отчество: ' + res.mName + '.<br>';
+//     mesWebsocket += 'Возраст: ' + res.age + '.<br>';
+//     document.getElementById('res').innerHTML = mesWebsocket
+// }
+//
+// socket.onerror = function(error) {
+//     document.getElementById('res').innerHTML = "Произошла ошибка: " + error.data;
+// };
 
-var socket = new WebSocket("ws://localhost:8081");
 
-socket.onmessage = function(event) {
-    var incomingMessage = event.data;
-    showMessage(incomingMessage);
+// var form = document.forms.form_local;
+// var outgoingMessage = {
+//     surname: form.surname.value,
+//     name: form.name.value,
+//     mName: form.mName.value,
+//     age: form.age.value
+// };
+// alert (outgoingMessage);
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("POST", "/getAge");
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    if (xhr.status != 200) {
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+        return;
+    }
+
+    var res = xhr.responseText;
+    // var mesAjax = 'Ответ приехал по Ajax:<br>';
+    // mesAjax += 'Фамилия: ' + res.surname + '.<br>';
+    // mesAjax += 'Имя: ' + res.name + '.<br>';
+    // mesAjax += 'Отчество: ' + res.mName + '.<br>';
+    // mesAjax += 'Возраст: ' + res.age + '.<br>';
+    document.getElementById('res').innerHTML = res;
 };
 
-function showMessage(message) {
-    var res = JSON.parse(message);
-    var mesWebsocket = 'Ответ приехал по Websocket:<br>';
-    mesWebsocket += 'Фамилия: ' + res.surname + '.<br>';
-    mesWebsocket += 'Имя: ' + res.name + '.<br>';
-    mesWebsocket += 'Отчество: ' + res.mName + '.<br>';
-    mesWebsocket += 'Возраст: ' + res.age + '.<br>';
-    document.getElementById('res').innerHTML = mesWebsocket
-}
-
-socket.onerror = function(error) {
-    document.getElementById('res').innerHTML = "Произошла ошибка: " + error.data;
-};
-
-
+xhr.send('Просто строка');
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var WebSocketServer = new require('ws');
 
 // подключенные клиенты
 var clients = {};
-var ajaxMessage = '';
+var age = '';
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({port: 8081});
@@ -20,6 +20,7 @@ webSocketServer.on('connection', function(ws) {
         console.log('В полученом сообщении имя: ' + res.name);
         console.log('В полученом сообщении отчество: ' + res.mName);
         console.log('В полученом сообщении возраст: ' + res.age);
+        age = res.age;
 
         for(var key in clients) {
             clients[key].send(JSON.stringify(res));
@@ -39,9 +40,19 @@ var fileServer = new Static.Server('.');
 http.createServer(function (req, res) {
     if (req.url == '/sendAjax') {
         req.on('data', function(chunk) {
-            ajaxMessage = chunk.toString();
+            var ajaxMessage = chunk.toString();
+            age = JSON.parse(ajaxMessage).age;
             console.log('Ajax с сервера отправляет: ' + ajaxMessage);
             res.end(ajaxMessage);
+        });
+
+    }
+    if (req.url == '/getAge') {
+
+        req.on('data', function(chunk) {
+            // ajaxMessage = chunk.toString();
+            console.log('Ajax с сервера отправляет: ' + age);
+            res.end(age);
         });
 
     }
